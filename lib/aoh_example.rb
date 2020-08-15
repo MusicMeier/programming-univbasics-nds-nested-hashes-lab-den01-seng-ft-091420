101b95d839049272eceaaf98c8cdd3d89830ce3b
Music Meier <musicm987@gmail.com>
2020-08-15 05:08:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
 KATE_G = { name:  "Kate Gompert", occupation: "None" },
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
-
+important_array
   
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
